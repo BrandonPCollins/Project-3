23c5f15ffb9df3d91f2b7ff40fd30cc88546624c
BrandonPCollins <bpcollins1995@gmail.com>
2023-06-23 16:46:01 +0100

    Initial Commit Testing

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pygame 
+
+name = input("What is your name?: \n")
+
+print(f"\n\nEllo, {name} I learning python\n\n\n\n")
