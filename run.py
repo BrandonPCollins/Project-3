da4245fd4ace2d5fb837ef7c9cea1fc066d500e7
BlackIronBadger <bpcollins1995@gmail.com>
2023-06-26 16:28:27 +0100

    Board Formatting

--- run.py
+++ run.py
@@ -22,11 +22,13 @@ letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
 def print_board(board):
     print(' A B C D E F G H')
-    print(' -------- ')
+    print(' - - - - - - - - ')
     row_number = 1
     for row in board:
         print("%d|%s" % (row_number, "|".join(row)))
         row_number += 1
+    print(' - - - - - - - - ')
+
 
 #Places ships on the board on game start 
 def create_ships(board):
