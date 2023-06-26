cbf141150a07fe506914b9eda4c9673168be504f
BlackIronBadger <bpcollins1995@gmail.com>
2023-06-26 15:22:18 +0100

    BattleShips Layout

--- run.py
+++ run.py
@@ -1,8 +1,29 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import pygame 
 
-name = input("What is your name?: \n")
+#For the Ships 
+HIDDEN_BOARD [[''] * 8 for x in range (8)] 
 
-print(f"\n\nEllo, {name} I learning python\n\n\n\n")
+#For Guessing
+GUESS_BOARD [[''] * 8 for x in range (8)] 
+
+#Convert guess letter string to num 
+letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+def print_board(board):
+    pass
+
+def create_ships():
+    pass 
+
+def get_ship_location():
+    pass 
+
+def count_hit_ships():
+    pass 
+
+create_ships()
+turns = 10
+
+while turns > 0:
