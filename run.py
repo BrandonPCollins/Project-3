8eee00e999ad538fd607fbb3a8ec98869d38149c
BlackIronBadger <bpcollins1995@gmail.com>
2023-06-26 16:35:17 +0100

    Adding Comments

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ GUESS_BOARD = [[' '] * 8 for x in range (8)]
 #Convert guess letter string to int 
 letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
+#Function for the creation of the board 
 def print_board(board):
     print(' A B C D E F G H')
     print(' - - - - - - - - ')
